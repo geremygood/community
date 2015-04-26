a6264c01fb77d2310307f94ed2179610b023e7af
Jeremy Mumenthaler <jeremy@simplehonestwork.com>
2015-04-26 10:04:46 -0700

    Part 3 | open id interface

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -19,3 +21,18 @@ def index():
                             title='home',
                             user=user,
                             events=events)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash('Login requested for OpenID-"%s"m remember_me=%s' %
+            (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+
+    return render_template('login.html',
+                            title='Sign In',
+                            form=form,
+                            providers=app.config['OPENID_PROVIDERS'])
