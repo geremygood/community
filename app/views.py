1a877a85da6ac93b59c0fefcf32f99c9a4ec6761
Jeremy Mumenthaler <jeremy@simplehonestwork.com>
2015-04-25 22:05:09 -0700

    step 2

--- app/views.py
+++ app/views.py
@@ -1,6 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello Crewel World."
+    user = {'nickname': 'Geremy Good'}  # fake user
+    events = [  # fake array of events
+        {
+            'host': {'name': 'Host 1'},
+            'name': 'Initial Event'
+        },
+        {
+            'host': {'name': 'Host 2'},
+            'name': 'Event Two: The second event'
+        }
+    ]
+    return render_template('index.html',
+                            title='home',
+                            user=user,
+                            events=events)
